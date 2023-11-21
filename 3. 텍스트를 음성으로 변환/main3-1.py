48c3ccd01c2a2830896372de23992aa88bc11c6b
parkgb97 <pkb1303@naver.com>
2023-11-21 10:23:24 +0900

    Guess Number Completed

--- 3. 텍스트를 음성으로 변환/main3-1.py
+++ 3. 텍스트를 음성으로 변환/main3-1.py
@@ -1,6 +1,11 @@
 from gtts import gTTS
 
+class TextToVoice:
+
+
+
+
 text = "안녕하세요. 파이썬과 40개의 작품들 입니다."
 
 tts = gTTS(text=text, lang='ko')
-tts.save(r"3. 텍스트를 음성으로 변환\hi.mp3")
+tts.save(r". / hi.mp3")
